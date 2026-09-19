83076efa66e24bd169869d3dae26c2f6f130965b
Nathan Chow <nathanchowcoding@gmail.com>
2026-09-18 20:30:28 -0400

    Add: is-palindrome - submission-3

---
+++ Data Structures & Algorithms/is-palindrome/submission-3.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        # abcba
+
+        l = 0
+        r = len(s) - 1
+
+        while l < r:
+            if not s[l].isalnum():
+                l += 1
+                continue
+            if not s[r].isalnum():
+                r -= 1
+                continue
+            if s[l].lower() == s[r].lower():
+                l += 1
+                r -= 1
+            else:
+                print(f"{s[l]} vs {s[r]}")
+                return False
+        
+        return True
